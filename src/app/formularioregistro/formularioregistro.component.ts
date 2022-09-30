ff0718543d323f6aa59b5dba112945b32d34df8a
311 <segara968@gmail.com>
2022-09-30 12:13:15 -0500

    se trabaja en angular

--- src/app/formularioregistro/formularioregistro.component.ts
+++ src/app/formularioregistro/formularioregistro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-formularioregistro',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioregistroComponent implements OnInit {
 
-  constructor() { }
+  formulario!:FormGroup;
+
+  constructor(public fabricaDiccionario:FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.formulario=this.inicializarFormulario()
+  }
+
+  public analizarFormulario():void{
+    console.log(this.formulario.value)
+  }
+
+  public inicializarFormulario():FormGroup{
+    return this.fabricaDiccionario.group({
+      iup:['nocturno',[Validators.required,Validators.minLength(6)]],
+      tiporemitente:['empresa',[Validators.required]],
+      idremitente:['123456789',[Validators.required]],
+      nombreremitente:['andercol',[Validators.required]],
+      deptoremitente:['antioquia',[Validators.required]],
+      munucipioremitente:['bello',[Validators.required]],
+      direccionremitente:['calle siempre viva #123',[Validators.required]],
+      tipodestinatario:['persona',[Validators.required]],
+      iddestinatario:['124578963',[Validators.required]],
+      nombredestinatario:['pipe',[Validators.required]],
+      deptodestinatario:['morroplancho',[Validators.required]],
+      munucipiodestinatario:['elrobadero',[Validators.required]],
+      direcciondestinatario:['esa misma #45-5',[Validators.required]],
+    
+    }) 
   }
 
 }
